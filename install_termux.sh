976ecb102ab99d2693c01d09f80a08180ce31590
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-09-26 22:44:34 +0530

    add comments. @Hackintosh5

--- install_termux.sh
+++ install_termux.sh
@@ -2,16 +2,27 @@
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
 
+# the below two lines were taken without
+# permission from 
+# https://github.com/friendly-telegram/friendly-telegram/blob/master/install.sh#L62
 if [ "$OSTYPE" = "linux-android" ]; then
   pkg install -y python git || { echo "installation of Python and GiT failed"; exit 2; }
+  # the above lines were taken without 
+  # permission from 
+  # https://github.com/friendly-telegram/friendly-telegram/blob/master/install.sh#L62
+  # create a virtual environment
   python3.7 -m venv venv
   . ./venv/bin/activate
   mkdir -p /sdcard/Telegram
+  # create and change into accessible sub directory
   cd /sdcard/Telegram
+  # install async
   pip install https://github.com/pyrogram/pyrogram/archive/asyncio.zip
   git clone https://github.com/SpEcHiDe/PyroGramUserBot
   cd PyroGramUserBot
+  # generate string session
   python3 GenerateStringSession.py
   cd ..
+  # cleanup
   rm -rf PyroGramUserBot
 fi

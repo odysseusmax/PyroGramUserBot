64b920e2b5ef01ff1d7cb0319ffef8725f611240
Shrimadhav U K <shrimadhavuk@gmail.com>
2019-09-27 17:09:53 +0530

    will squash these later!!!

--- install_termux.sh
+++ install_termux.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
+
 # the entire source code is GPL, except this file,
 # which is AGPL
 # (c) 2017
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # the below two lines were taken without
 # permission from 

afaad84b2e7ed99312ffaa640b1783098611d3e4
odysseusmax <royschristy@gmail.com>
2019-09-29 07:57:07 +0000

    testing autopic

--- pyrobot/plugins/autopic.py
+++ pyrobot/plugins/autopic.py
@@ -4,17 +4,19 @@ Syntax: .autopic"""
 from pyrogram import Client, Filters
 from PIL import Image, ImageFont, ImageDraw 
 
-import io
 import os
 import time
-import traceback
+import asyncio
+import logging
 import aiofiles
+import datetime
+import traceback
 
 from pyrobot import MAX_MESSAGE_LENGTH, COMMAND_HAND_LER
 
-import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 logger = logging.getLogger(__name__)
 
 UPDATE_PIC = False
@@ -43,30 +45,31 @@ async def autopic(client, message):
         logger.info('profile pic updation started')
         
         while UPDATE_PIC:
-            if int(time.time())%60 == 0:
-                base_pic = "pics/base_profile_pic.jpg"
+            base_pic = "pics/base_profile_pic.jpg"
                 
-                new_pic = "pics/new_profile_pic.jpg"
+            new_pic = "pics/new_profile_pic.jpg"
                 
-                img = Image.open(base_pic)
+            img = Image.open(base_pic)
                 
-                width, height = img.size
+            width, height = img.size
                 
-                draw = ImageDraw.Draw(img)
+            draw = ImageDraw.Draw(img)
                 
-                tim = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(minutes=30, hours=5)))
+            tim = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(minutes=30, hours=5)))
                 
-                content = f"@odysseusmax\nDate: {tim.day}.{tim.month}.{tim.year}\nTime: {tim.hour}:{tim.minute}:{tim.second}\nUTC+5:30"
+            content = f"@odysseusmax\nDate: {tim.day}.{tim.month}.{tim.year}\nTime: {tim.hour}:{tim.minute}:{tim.second}\nUTC+5:30"
                 
-                draw.text((0, width), content,(255,255,255),font=font)
+            draw.text((0, width), content,(255,255,255),font=font)
                 
-                img.save(new_pic)
+            img.save(new_pic)
                 
-                await client.set_profile_photo(new_pic)
+            await client.set_profile_photo(new_pic)
                 
-                await aiofiles.os.remove(new_pic)
+            await aiofiles.os.remove(new_pic)
                 
-                logger.info(f'profile pic updated at {tim}')
+            logger.info(f'profile pic updated at {tim}')
+            
+            await asyncio.sleep(60)
         
         logger.info(f'profile pic updation stopped')
     except Exception:

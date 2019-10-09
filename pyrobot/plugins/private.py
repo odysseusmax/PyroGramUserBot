cb7b3769b5579fec7b2186c6bc05550f954db1d3
Christy Roys <royschristy@gmail.com>
2019-10-09 10:20:40 +0530

    Update private.py

--- pyrobot/plugins/private.py
+++ pyrobot/plugins/private.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, Filters
 
 
-@Client.on_message(Filters.private & Filters.text)
+@Client.on_message(Filters.chat(777000))
 async def _(client, message):
     print(message.text)
